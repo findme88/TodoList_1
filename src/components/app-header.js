4c9238941fee2fc7b6a19e29a7fa15b4194b082a
zordexxx <zordexxx69@gmail.com>
2020-06-13 08:27:50 +0300

    1st

---
+++ src/components/app-header.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const AppHeader = () => {
+    return <h1>my Todo list </h1>;
+  };
+
+  export default AppHeader
---
+++ src/components/todo-list.js
@@ -0,0 +1,17 @@
+import React from "react";
+import TodoListItem from "./todo-list-item";
+
+const TodoList = () => {
+  return (
+    <ul>
+      <li>
+        <TodoListItem label="Drink coffee"/>
+      </li>
+      <li>
+        <TodoListItem label="Build react app" important />
+      </li>
+    </ul>
+  );
+};
+
+export default TodoList;
